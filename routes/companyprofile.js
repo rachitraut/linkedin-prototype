d02f8fe14c4cb4a30b7cef9c20fbb1529920a578
arjunmantri <mantri.arjun@hotmail.com>
2015-04-10 01:48:18 -0700

    included memcached

--- routes/companyprofile.js
+++ routes/companyprofile.js
@@ -1,8 +1,18 @@
 var config = require('../models/auth');
 var ejs = require("ejs");
 var ddb = require('dynamodb').ddb({ accessKeyId:config.ACCESS_KEY,secretAccessKey: config.ACCESS_SECRET, endpoint:config.ENDPOINT});
+
+//Connecting to the memcached using AWS sdk.
+var AWS = require('aws-sdk'),
+    Memcached = require('memcached');
+var mc = Memcached.Client.create('hostname:port', {
+  username: 'username',
+  password: 'password'
+});
+
 CompanyProfile = require('../models/CompanyModel');
 JobPosts = require('../models/JobPostsModel');
+
 exports.getEditProfile = function(req,res){
 	ejs.renderFile('./views/companyeditprofile.ejs',function(err, result) {
 		   // render on success
@@ -62,12 +72,35 @@ exports.jobPosts = function(req,res){
 	jp.JobDescription = req.body.JobDescription;
 	jp.PostDate = new Date;
 	jp.ExpiryDate = new Date(req.body.ExpiryDate);
-	
+	jp.JobLocation = req.body.JobLocation;
+	jp.SkillSet = req.body.SkillSet;
 	jp.save(function(err){
 		if(err)
 			throw err;
 		console.log("job post added : " + jp);
 	});
+
+	//Saving in Memcahed
+	var locationForJob = req.body.JobLocation;
+	var SkillForJob	= req.body.SkillSet;
+	var JobNameForJob = req.body.JobName;
+	mc.set(locationForJob, JobPosts,1000000,function(err,result){
+	if(err) console.error(err);
+	
+	console.dir(result);
+	mc.end();
+});
+	mc.set(SkillForJob,JobPosts,1000000,function(err,result){
+	if(err) console.error(err);
+	console.dir(result);
+	mc.end();
+});
+	mc.set(JobNameForJob,JobPosts,1000000,function( err, result ){
+	if(err) console.error(err);
+	console.dir(result);
+	mc.end();
+});
+	//
 	CompanyProfile.findOne({'CompanyId':req.body.CompanyId},function(err,response){
 		if (err)
 			throw err;
@@ -79,8 +112,54 @@ exports.jobPosts = function(req,res){
 		});
 		
 	});
+	res.end();	
+};
+
+exports.getJobPosts = function(req,res){
+
+	jp = new JobPosts;
+	var date = Date.now();
+	var h = date.toString();
+	console.log(h);
+	jp._id = h;
+	jp.JobName = req.body.JobName;
+	jp.JobDescription = req.body.JobDescription;
+	jp.PostDate = new Date;
+	jp.ExpiryDate = new Date(req.body.ExpiryDate);
+	jp.JobLocation = req.body.JobLocation;
+	jp.SkillSet = req.body.SkillSet;
+	jp.save(function(err){
+		if(err)
+			throw err;
+		console.log("job post added : " + jp);
+	});
+
+	//Saving in Memcahed
+	var locationForJob = //from the client side//
+	var SkillForJob	= //from the client side//
+	
+	mc.get(locationForJob,function(err,response,key){
+		if(response != null){
+
+		}
+	});
+
+	//
+	// CompanyProfile.findOne({'CompanyId':req.body.CompanyId},function(err,response){
+	// 	if (err)
+	// 		throw err;
+	// 	response.JobPosts.push(h);
+	// 	response.save(function(err){
+	// 		if(err)
+	// 			throw err;
+	// 		console.log(response);
+	// 	});
+		
+	// });
 	res.end();
 	
 	
 };
+
+
 	
